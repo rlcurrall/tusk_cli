e47a7fba8ad55828f9e50215073453e3e87e407a
Robb Currall <robb@hipecommerce.com>
2021-06-13 16:31:04 -0400

    use anyhow for error handling

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,43 @@
+use anyhow::{anyhow, Result};
 use clap::{load_yaml, App};
 use std::fs;
+use std::option::Option;
 use tusk_lexer::Lexer;
 use tusk_parser::Parser;
 
-fn output_file_ast(filepath: &str, format: &str, out: &str) {
-    match fs::read_to_string(filepath) {
-        Err(error) => {
-            eprintln!("Error: {}", error);
-            std::process::exit(1);
-        }
-        Ok(contents) => {
-            let lexer = Lexer::new(contents.as_str());
-            let mut parser = Parser::new(lexer);
+fn output_file_ast(filepath: &str, format: Option<&str>, out: Option<&str>) -> Result<()> {
+    let contents = fs::read_to_string(filepath)?;
+    let lexer = Lexer::new(contents.as_str());
+    let mut parser = Parser::new(lexer);
 
-            match parser.all() {
-                Err(error) => {
-                    eprintln!("Error: {}", error);
-                    std::process::exit(1);
-                }
-                Ok(program) => {
-                    let ast_str = match format {
-                        "json" => serde_json::to_string_pretty(&program).unwrap(),
-                        "debug" => format!("{:#?}", program),
-                        _ => {
-                            eprintln!("Unsupported format.");
-                            std::process::exit(1)
-                        }
-                    };
+    let program = parser.all().unwrap();
 
-                    match out {
-                        "stdout" => println!("{}", ast_str),
-                        _ => fs::write(out, ast_str).unwrap(),
-                    }
-                }
-            }
-        }
-    }
+    let ast_str = match format {
+        None => format!("{:#?}", program),
+        Some("json") => serde_json::to_string_pretty(&program)?,
+        Some(_) => return Err(anyhow!("Unsupported format.")),
+    };
+
+    match out {
+        Some(path) => fs::write(path, ast_str)?,
+        None => println!("{}", ast_str),
+    };
+
+    Ok(())
 }
 
-fn main() {
+fn main() -> Result<()> {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
     match matches.subcommand() {
         ("ast", Some(ast_matches)) => {
             let filepath = ast_matches.value_of("file").unwrap();
-            let format = ast_matches.value_of("format").unwrap();
-            let out = ast_matches.value_of("out").unwrap();
+            let format = ast_matches.value_of("format");
+            let out = ast_matches.value_of("out");
 
-            output_file_ast(filepath, format, out);
+            output_file_ast(filepath, format, out)
         }
-        _ => unimplemented!(),
+        _ => Err(anyhow!("Unimplemented!")),
     }
 }
